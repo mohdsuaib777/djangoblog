8c4be09e583b3e4c6fee997d767d0fcdffbbb108
mohdsuaib777 <114831375+mohdsuaib777@users.noreply.github.com>
2023-03-26 12:25:10 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY=os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=str(os.environ.get("DEBUG"))=='True'
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['https://mydajngoapp.netlify.app']
 
 
 # Application definition
